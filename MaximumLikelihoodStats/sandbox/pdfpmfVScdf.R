372d8e214cb01d74e0ed73ba62f4ab0c4c4ebe9e
Zongyi <zh2720@ic.ac.uk>
2021-03-26 16:35:46 +0000

    delete some files

--- MaximumLikelihoodStats/sandbox/pdfpmfVScdf.R
+++
@@ -1,4 +0,0 @@
-cdf_7_6 <- (pbinom(q = 7,size = 10,prob = 0.5,log.p = FALSE)-pbinom(q = 6,size = 10,prob = 0.5,log.p = FALSE))
-test <- dbinom(x = 7,size = 10,prob = 0.5, log = FALSE)
-cdf_7_6
-test
